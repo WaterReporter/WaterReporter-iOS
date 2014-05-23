7f9ce63788bc0a89759218ac5c2159e2fb27fce3
Joshua Powell <joshua@viableindustries.com>
2014-05-23 11:20:11 -0400

    Fixed a bug that was causing the 'previously submitted image to be deleted when a user would post 2 or more reports in a row without existing the application;

--- WaterReporter/WaterReporter/VIReportsTableViewController.m
+++ WaterReporter/WaterReporter/VIReportsTableViewController.m
@@ -37,9 +37,7 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    NSLog(@"PREPARE");
-    
+{    
     if ([[segue identifier] isEqualToString:@"viewReport"]) {
         VISingleReportTableViewController *upcoming = [segue destinationViewController];
         
