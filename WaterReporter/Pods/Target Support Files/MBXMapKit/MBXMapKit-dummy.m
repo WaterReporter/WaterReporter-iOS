109a0dde0527a171a097ea996ab30e5e00ca4846
Joshua Powell <joshua@viable.io>
2016-06-06 10:03:40 -0400

    Removed legacy application files, artwork files, and vendor libraries; Updated the .gitignore file to better exclude files that need not be tracked;

--- WaterReporter/Pods/Target Support Files/MBXMapKit/MBXMapKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MBXMapKit : NSObject
-@end
-@implementation PodsDummy_MBXMapKit
-@end
--- WaterReporter/Water-Reporter/Group.c
+++
@@ -1,9 +0,0 @@
-//
-//  Group.c
-//  Water-Reporter
-//
-//  Created by Joshua Powell on 2/17/16.
-//  Copyright © 2016 Viable Industries, L.L.C. All rights reserved.
-//
-
-#include "Group.h"
