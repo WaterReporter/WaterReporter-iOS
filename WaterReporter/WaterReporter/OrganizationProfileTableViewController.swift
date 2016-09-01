a9c433858228e96cbf7dd2d61927c04a041c661d
Joshua Powell <joshua@viable.io>
2016-09-01 11:20:04 -0400

    Added new Organization Profile Table View Controller and Storyboard;

---
+++ WaterReporter/WaterReporter/OrganizationProfileTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizationProfileTableViewController.swift
+//  Water-Reporter
+//
+//  Created by Viable Industries on 9/1/16.
+//  Copyright © 2016 Viable Industries, L.L.C. All rights reserved.
+//
+
+import Alamofire
+import Foundation
+import UIKit
+
+class OrganizationProfileTableViewController: UITableViewController {
+
+    
+    
+}
